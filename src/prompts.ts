a0283f4ba32e85b2bb16298d1f836fe9d1faf9d5
Allan <martinezallan123@gmail.com>
2022-10-06 18:12:19 -0700

    collection of prompts

---
+++ src/prompts.ts
@@ -0,0 +1,73 @@
+ 
+ const prompts = {
+  easy: [
+    {
+      prompt:"I mean, yes! They may have the appearance of riches. But, beneath the clothes, we find a man. And beneath the man, we find his nucleus.",
+      author: "Nacho Libre",
+    },
+    {
+      prompt: "Okay, fine. I had to fight a guy to be with her, okay? I fought a crazy, 80-foot-tall purple-suited dude, and I had to fight 96 guys to get to him. He was flying and shooting lightning bolts from his eyes, okay?",
+      author: "Scott Pilgrim"
+    },
+    {
+      prompt: "Someday, I will repay you. Unless, of course, I can't find you or if I forget.",
+      author: "Shrek"
+    },
+    {
+      prompt: "My name is Nobody. That is what I am called by my mother and father and by all my friends",
+      author: "Odysseus"
+    },
+    {
+      prompt: "I knew exactly what to do, but in a much more real sense I had no idea what to do",
+      author: "Michael Scott"
+    }
+  ],
+  medium: [
+    {
+      prompt: "Look at it! It's a once in a lifetime opportunity, man! Let me go out there and let me get one wave, just one wave before you take me in. I mean, come on man, where I am I gonna go? Cliffs on both sides! I'm not gonna paddle my way to New Zealand! Come on, compadre. Come on!",
+      author: "Bodhi"
+    },
+    {
+      prompt: "I think the best way to tell the story is by starting at the end, briefly, then going back to the beginning, and then periodically returning to the end, maybe giving different characters' perspectives throughout.",
+      author: "The Other Guys"
+    },
+    {
+      prompt: "My team. Allow me the pleasure of introducing you to Blade. Laser. Blazer. I believe you've met my fitness consigliere, Me'Shell",
+      author: "White Goodman"
+    },
+    {
+      prompt: "I wanna do a cartwheel. But real casual-like. Not enough to make a big deal out of it, but I know everyone saw it. One stunning, gorgeous cartwheel.",
+      author: "Creed Bratton"
+    },
+    {
+      prompt: "Now, uh, Ms. Vito, being an expert on general automotive knowledge, can you tell me... what would the correct ignition timing be on a 1955 Bel Air Chevrolet, with a 327 cubic-inch engine and a four-barrel carburetor?",
+      author: "Jim Trotter"
+    },
+  ],
+  hard: [
+    {
+      prompt: "Okay, well... So as soon as there was the first wave, right? - there was the first surfer. All they needed was, like, um, a piece of driftwood, a block of ice or something like that, and then they were off. And they were hooked, man. They were hooked. Up 'till recent times, you had your old dudes, you know, your hang-six cats, these old guys used to lay down with these huge, humongous boards. These guys were the pioneers, but nobody saw what surfing could really be until Big Z did it.",
+      author: "Cody Maverick"
+    },
+    {
+      prompt: "4:00, wallow in self-pity. 4:30, stare into the abyss. 5:00, solve world hunger, tell no one. 5:30, jazzercize; 6:30, dinner with me. I can't cancel that again. 7:00, wrestle with my self-loathing. I'm booked. Of course, if I bump the loathing to 9, I could still be done in time to lay in bed, stare at the ceiling and slip slowly into madness.",
+      author: "The Grinch"
+    },
+    {
+      prompt: "'Cause Chevy didn't make a 327 in '55, the 327 didn't come out till '62. And it wasn't offered in the Bel Air with a four-barrel carb till '64. However, in 1964, the correct ignition timing would be four degrees before top-dead-center.",
+      author: "Mona Lisa Vito"
+    },
+    {
+      prompt: "When I first came here, this was all swamp. Everyone said I was daft to build a castle on a swamp, but I built in all the same, just to show them. It sank into the swamp. So I built a second one. And that one sank into the swamp. So I built a third. That burned down, fell over, and then sank into the swamp. But the fourth one stayed up. And that’s what you’re going to get, Son, the strongest castle in all of England.",
+      author: "Monty Python and the Holy Grail"
+    },
+    {
+      prompt: "I wrestled with an alligator, I done tussled with a whale; handcuffed lightning, thrown thunder in jail; only last week, I murdered a rock, injured a stone, hospitalized a brick; I'm so mean I make medicine sick",
+      author: "Muhammad Ali"
+    },
+  ],
+};
+
+  
+
+export {prompts}
